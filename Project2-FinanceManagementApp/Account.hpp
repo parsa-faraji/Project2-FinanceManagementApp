ae1f887e284a38e97b7650ae64dee8e267690e8c
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-17 22:53:59 -0800

    implement Account header

--- Project2-FinanceManagementApp/Account.hpp
+++ Project2-FinanceManagementApp/Account.hpp
@@ -1,13 +1,33 @@
-//
-//  Account.hpp
-//  Project2-FinanceManagementApp
-//
-//  Created by Parsa Faraji on 11/17/24.
-//
-
 #ifndef Account_hpp
 #define Account_hpp
 
-#include <stdio.h>
+#include <iostream>
+
+struct Account {
+    long long accountNumber;
+    double balance;
+
+    // constructors
+    Account();
+    Account(long long accountNumber, double balance);
+
+    // setters
+    void setAccountNumber(long long newAccountNumber);
+    void setBalance(double newBalance);
+
+    // getters
+    long long getAccountNumber() const;
+    double getBalance() const;
+
+    // utility functions
+    void deposit(double amount);
+    void withdraw(double amount);
+
+    // equality operator
+    bool operator==(const Account& other) const;
+
+    // stream overload
+    friend std::ostream& operator<<(std::ostream& os, const Account& account);
+};
 
 #endif /* Account_hpp */
