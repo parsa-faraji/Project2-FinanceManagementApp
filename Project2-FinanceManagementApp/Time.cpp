49d4d78602ca8bd8b26d84ca399d7a429d9dab0e
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-17 22:50:41 -0800

    implement the Time struct

--- Project2-FinanceManagementApp/Time.cpp
+++ Project2-FinanceManagementApp/Time.cpp
@@ -1,8 +1,43 @@
-//
-//  Time.cpp
-//  Project2-FinanceManagementApp
-//
-//  Created by Parsa Faraji on 11/17/24.
-//
-
 #include "Time.hpp"
+#include <stdexcept>
+
+// default constructor
+Time::Time() : hour(0), minute(0) {}
+
+// parameterized constructor
+Time::Time(short int hour, short int minute) : hour(hour), minute(minute) {
+    if (!isValidTime()) {
+        throw std::invalid_argument("Invalid time provided.");
+    }
+}
+
+void Time::setHour(short int newHour) {
+    if (newHour < 0 || newHour > 23) {
+        throw std::invalid_argument("Hour must be between 0 and 23.");
+    }
+    hour = newHour;
+}
+
+void Time::setMinute(short int newMinute) {
+    if (newMinute < 0 || newMinute > 59) {
+        throw std::invalid_argument("Minute must be between 0 and 59.");
+    }
+    minute = newMinute;
+}
+
+short int Time::getHour() const { return hour; }
+short int Time::getMinute() const { return minute; }
+
+bool Time::isValidTime() const {
+    return hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59;
+}
+
+bool Time::operator==(const Time& other) const {
+    return hour == other.hour && minute == other.minute;
+}
+
+std::ostream& operator<<(std::ostream& os, const Time& time) {
+    os << (time.hour < 10 ? "0" : "") << time.hour << ":"
+       << (time.minute < 10 ? "0" : "") << time.minute;
+    return os;
+}
--- Project2-FinanceManagementApp/Time.hpp
+++ Project2-FinanceManagementApp/Time.hpp
@@ -1,13 +1,32 @@
-//
-//  Time.hpp
-//  Project2-FinanceManagementApp
-//
-//  Created by Parsa Faraji on 11/17/24.
-//
-
 #ifndef Time_hpp
 #define Time_hpp
 
-#include <stdio.h>
+#include <iostream>
+
+struct Time {
+    short int hour;    // 0 to 23
+    short int minute;  // 0 to 59
+
+    // constructors
+    Time();
+    Time(short int hour, short int minute);
+
+    // setters
+    void setHour(short int newHour);
+    void setMinute(short int newMinute);
+
+    // getters
+    short int getHour() const;
+    short int getMinute() const;
+
+    // utility function
+    bool isValidTime() const;
+
+    // equality operator
+    bool operator==(const Time& other) const;
+
+    // stream overload
+    friend std::ostream& operator<<(std::ostream& os, const Time& time);
+};
 
 #endif /* Time_hpp */
