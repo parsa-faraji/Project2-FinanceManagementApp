8e5f794fadec436a5bdf658ca5ea3250b57b02b0
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-17 22:52:36 -0800

    implement Transaction.hpp

--- Project2-FinanceManagementApp/Transaction.hpp
+++ Project2-FinanceManagementApp/Transaction.hpp
@@ -1,13 +1,50 @@
 //
 //  Transaction.hpp
-//  Project2-FinanceManagementApp
+//  FinanceManagementApp
 //
-//  Created by Parsa Faraji on 11/17/24.
+//  Created by Parsa Faraji on 11/15/24.
 //
 
 #ifndef Transaction_hpp
 #define Transaction_hpp
 
-#include <stdio.h>
+#include <string>
+#include "Date.h"
+#include "Account.h"
+#include "Time.h"
+
+using std::string;
+
+// enum for transaction methods
+enum class TransactionMethod {
+    InStore,
+    Online
+};
+
+// transaction structure
+struct Transaction {
+    string transactionType;  // e.g., "Purchase" or "Deposit"
+    Date date;
+    Time time;
+    Account account;
+    double amount;
+    string category;
+    TransactionMethod method;
+
+    // utility functions
+    Date getDate() const { return date; }
+    double getAmount() const { return amount; }
+    string getCategory() const { return category; }
+    TransactionMethod getMethod() const { return method; }
+    bool isValidTransaction() const;
+
+    // csv serialization
+    string toCSV() const;
+    static Transaction fromCSV(const string& csvLine);
+
+    // overloaded operators
+    bool operator==(const Transaction& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Transaction& transaction);
+};
 
 #endif /* Transaction_hpp */
